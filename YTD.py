613b81e5a08cc64cbfb16270f126d638156bd68b
Rafael Aurelius Campos <therobotfighter.009@gmail.com>
2026-05-05 12:07:54 +0800

    Add progress bar

--- YTD.py
+++ YTD.py
@@ -1,8 +1,25 @@
 import yt_dlp 
 import PySimpleGUI as sg
+import threading
 
 # Selecting filepath
 output_directory = r'C:\Users\Home\Downloads'
+
+# Progress Bar
+def progress_hook(d, window):
+    if d['status'] == 'downloading':
+        percent = d.get('_percent_str', '0%').strip()
+        try: percent_value = float(percent.replace('%', "").strip())
+        except: 
+            percent_value = 0
+        
+        # Send Progress to GUI
+        window.write_event_value('-PROGRESS-', percent_value)
+        window.write_event_value('-STATUS-', f"Downloading... {percent}")
+    elif d['status'] == 'finished':
+        window.write_event_value('-STATUS-', "Processing")
+
+
 
 def download_yt_video(url):
     ydl_opts ={
@@ -10,20 +27,22 @@ def download_yt_video(url):
         'noplaylist' : True,
         'paths' : {'home': output_directory},
         'logger': None,
-        'verbose': True
+        'verbose': True,
+        'progress_hooks': [lambda d: progress_hook(d, window)]
     }
 
     try: 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
-        return "Download Complete!"
+        window.write_event_value('-DONE-', "Download Complete!")
     except Exception as e:
-        return f"Error: {e}"
+        window.write_event_value('-DONE-', f"Error: {e}")
     
 # Window Design
 layout = [
     [sg.Text("Please enter link: ")], 
-    [sg.InputText(key="-URL-", size=(50,1)), sg.Button('Download'), sg.Button('Cancel')], 
+    [sg.Input(key="-URL-", size=(50,1)), sg.Button('Download'), sg.Button('Cancel')], 
+    [sg.ProgressBar(100, orientation='h', size=(40,20), key='-PROG-')],
     [sg.Text("", key="-STATUS-")]
     ]
 
@@ -36,16 +55,30 @@ while True:
     if event == sg.WINDOW_CLOSED:
         break
 
-    if event == "Download":
+    elif event == "Download":
         url = values ["-URL-"].strip()
         if url:
-            window["-STATUS-"].update("Downloading...")
-            window.refresh()
+            window['-PROG-'].update(0)
+            window['-STATUS-'].update("Starting Download...")
             
-            result = download_yt_video(url)
-            window["-STATUS-"].update(result)
-        else: 
-            window["-STATUS-"].update("Please enter link: ")
+            # Download in separate thread
+            threading.Thread(
+                target =download_yt_video,
+                args=(url, window),
+                daemon=True
+            ).start
+        else:
+            window['-STATUS-'].update("Please enter a Link: ")
 
+    elif event == "-PROGESS-":
+        window['-PROG-'].update(values['-PROGESS-'])
+
+    elif event == "-STATUS-":
+        window['-STATUS-'].update(values['-STATUS-'])
+        
+    elif event == '-DONE-':
+        window['-STATUS-'].update(values['-DONE-'])
+        window['-PROG-'].update(100)
+    
 window.close()
 
